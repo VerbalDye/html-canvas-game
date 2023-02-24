56a45c0d9afaba666ebb097df81ed60ab0dd7b19
VerbalDye <dhunts258@gmail.com>
2023-02-24 11:42:33 -0500

    added game and player object

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,55 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
-
-console.log(canvas);
-console.log(ctx);
-
-ctx.fillStyle = "rgb(200, 0, 0)";
-ctx.fillRect(10, 10, 50, 50);
-
-ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-ctx.fillRect(30, 30, 50, 50);
-
-for (var i = 1; i < 600; i++) {
+window.addEventListener('load', function() {
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext('2d');
     
-    ctx.clearRect(0, 0, 650, 500)
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect(30, 30, i, 50);
-}
+    canvas.width = 1280;
+    canvas.height = 720;
+
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white';
+
+    class Player {
+        constructor(game) {
+            this.game = game;
+            this.collisionX = this.game.width * 0.5;
+            this.collisionY = this.game.height * 0.5;
+            this.collisionRadius = 30;
+        }
+        draw(context) {
+            context.beginPath();
+            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI *2);
+            context.save();
+            context.globalAlpha = 0.5;
+            context.fill();
+            context.restore();
+            context.stroke();
+        }
+    }
+
+    class Game {
+        constructor(canvas) {
+            this.canvas = canvas;
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            this.player = new Player(this);
+
+            //event listeners
+            window.addEventListener('mousedown', function() {
+                console.log('mousedownh')
+            })
+        }
+        render(context) {
+            this.player.draw(context);
+
+        }
+    }
+
+    const game = new Game(canvas);
+    game.render(ctx);
+    console.log(game);
+
+    function animate() {
+
+    }
+});
